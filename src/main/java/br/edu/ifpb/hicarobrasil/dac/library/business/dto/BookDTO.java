eebb4dc778c0c5cb4a44436fcb586865b26f8744
hicaro_brasil <hicaro.brasil@hotmail.com>
2023-09-27 02:22:55 -0300

    :recycle: refactor: change constructor bookDTO

--- src/main/java/br/edu/ifpb/hicarobrasil/dac/library/business/dto/BookDTO.java
+++ src/main/java/br/edu/ifpb/hicarobrasil/dac/library/business/dto/BookDTO.java
@@ -1,4 +1,4 @@
 package br.edu.ifpb.hicarobrasil.dac.library.business.dto;
 
-public record BookDTO(String title, String year, String publisher, String author) {
+public record BookDTO(String title, String author, String year, String publisher) {
 }
