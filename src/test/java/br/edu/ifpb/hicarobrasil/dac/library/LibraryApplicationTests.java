50bd19e7e1ad5eb925ed01990eb83efdba9947ec
Hicaro_Brasil <hicaro.brasil@hotmail.com>
2023-10-18 05:35:31 -0300

    :boom: fix: revert tests

--- src/test/java/br/edu/ifpb/hicarobrasil/dac/library/LibraryApplicationTests.java
+++ src/test/java/br/edu/ifpb/hicarobrasil/dac/library/LibraryApplicationTests.java
@@ -1,13 +1,8 @@
 package br.edu.ifpb.hicarobrasil.dac.library;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class LibraryApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
