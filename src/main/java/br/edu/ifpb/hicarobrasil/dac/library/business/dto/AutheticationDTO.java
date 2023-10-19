00ee558520529e2d1a84c0ccfadc6a9bc551ee84
Hicaro_Brasil <hicaro.brasil@hotmail.com>
2023-10-19 16:10:57 -0300

    :sparkles: feat: add new user dtos

---
+++ src/main/java/br/edu/ifpb/hicarobrasil/dac/library/business/dto/AutheticationDTO.java
@@ -0,0 +1,5 @@
+package br.edu.ifpb.hicarobrasil.dac.library.business.dto;
+
+public record AutheticationDTO(String login, String password) {
+    
+}
---
+++ src/main/java/br/edu/ifpb/hicarobrasil/dac/library/business/dto/LoginDTO.java
@@ -0,0 +1,5 @@
+package br.edu.ifpb.hicarobrasil.dac.library.business.dto;
+
+public record LoginDTO(String token) {
+    
+}
