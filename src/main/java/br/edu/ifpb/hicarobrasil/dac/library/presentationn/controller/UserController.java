ae8a2483a4e9d54b2f58bccba26264783ed9a100
Hicaro_Brasil <hicaro.brasil@hotmail.com>
2023-10-19 16:13:16 -0300

    :sparkles: feat: finish userController

--- src/main/java/br/edu/ifpb/hicarobrasil/dac/library/presentationn/controller/UserController.java
+++ src/main/java/br/edu/ifpb/hicarobrasil/dac/library/presentationn/controller/UserController.java
@@ -1,7 +1,13 @@
 package br.edu.ifpb.hicarobrasil.dac.library.presentationn.controller;
 
+import static org.mockito.ArgumentMatchers.refEq;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,38 +15,48 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.edu.ifpb.hicarobrasil.dac.library.business.dto.AutheticationDTO;
+import br.edu.ifpb.hicarobrasil.dac.library.business.dto.LoginDTO;
 import br.edu.ifpb.hicarobrasil.dac.library.business.dto.UserDTO;
 import br.edu.ifpb.hicarobrasil.dac.library.business.services.UserService;
+import br.edu.ifpb.hicarobrasil.dac.library.infra.security.TokenService;
+import br.edu.ifpb.hicarobrasil.dac.library.model.entity.User;
+import jakarta.validation.Valid;
 
 @RestController
-@RequestMapping("/user")
+@RequestMapping("auth")
 @CrossOrigin(origins = "http://localhost:3000")
 public class UserController {
 
     @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
     private UserService userService;
+
+    @Autowired
+    private TokenService tokenService;
 
     @RequestMapping("/cadastrar")
     @PostMapping
-    public ResponseEntity<UserDTO> create(@RequestBody UserDTO userDTO){
-        try {
-            UserDTO user = userService.create(userDTO);   
-            return new ResponseEntity<>(user, HttpStatus.CREATED);
-        } catch (Exception e) {
+    public ResponseEntity<LoginDTO> cadastraResponseEntity(@RequestBody @Valid UserDTO userDTO){
+        if(userService.findByLogin(userDTO.login())!= null)
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-        }
-
-    }
+        String encryptedPassword = new BCryptPasswordEncoder().encode(userDTO.password());    
+        UserDTO cryptUserDTO = new UserDTO(userDTO.id(),userDTO.name(), userDTO.email() ,userDTO.login(), encryptedPassword, userDTO.role());
+        userService.create(cryptUserDTO);
+        return new ResponseEntity<>(HttpStatus.OK);
+    } 
 
     @RequestMapping("/login")
     @PostMapping
-    public ResponseEntity<UserDTO> login(@RequestBody UserDTO userDTO){
-        try {
-            UserDTO user = userService.findByID(userDTO.id());   
-            return new ResponseEntity<>(user, HttpStatus.OK);
-        } catch (Exception e) {
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-        }
+    public ResponseEntity<LoginDTO> login(@RequestBody @Valid AutheticationDTO userDTO){
+        UsernamePasswordAuthenticationToken usernamePassword = new UsernamePasswordAuthenticationToken(userDTO.login(), userDTO.password());
+        Authentication auth = authenticationManager.authenticate(usernamePassword);
+        String token = tokenService.generateToken((User) auth.getPrincipal());
+        
+        return new ResponseEntity<>(new LoginDTO(token), HttpStatus.OK);
     }
+
 
 }
